d6e04e4d873d53dda5bcc5b0c4c857b979ec9363
Venicios <127702005+v3nicios@users.noreply.github.com>
2026-09-15 17:25:48 -0400

    adiciona componente Home e atualiza App para usá-lo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import { Home } from './pages/Home';
+
 export  function App() {
   return (
     <View>
-      <Text>Home</Text>
-      
+       <Home/>
     </View>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+export const Home = () => {
+
+  return(
+    <View>
+        <Text>Home page</Text>
+    </View>
+  )
+}

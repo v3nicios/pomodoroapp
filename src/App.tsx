8624c8c2126ff10725bc3a6bb22fa325d5d8804e
Venicios <127702005+v3nicios@users.noreply.github.com>
2026-09-15 17:06:42 -0400

    Reorganiza estrutura do projeto: move App.tsx para src e atualiza index.ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export  function App() {
+  return (
+    <View>
+      <Text>Home</Text>
+      
+    </View>
+  );
+}
+
+
+

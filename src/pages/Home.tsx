7077edf6e4e1bb823bdafdcbf1869a8f78b78d01
v3nicios <127702005+v3nicios@users.noreply.github.com>
2026-09-16 20:36:37 -0400

    Refactor code structure for improved readability and maintainability

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,23 +8,44 @@ export const Home = () => {
     const navigation = useNavigation<TNavigationScreenProps>();
 
     return (
-        <View>
-             <View style={styles.progressContainer}>
-                
-            <AnimatedCircularProgress
-                size={160}
-                width={7}
-                fill={90}
-                rotation={0}
-                tintColor={Theme.colors.divider}
-                backgroundColor={Theme.colors.primary}
-                children={()=> (
-                    <Text style={styles.progressText} >
-                        12:45
-                    </Text>
-                )}
-                />
+        <View style={styles.container} >
+
+            <View style={styles.titlegroup} >
+                <View style={styles.titlecontainer}  >
+
+                    <Text style={styles.titletext}  >Pomodoro</Text>
                 </View>
+
+                <View style={styles.statecontainer}  >
+                    <Text style={styles.statetext}> Hora de se Concentrar</Text>
+                    <Text style={styles.statetext}> Pausa curta</Text>
+
+                    <Text style={styles.statetext}> Pausa longa</Text>
+
+                    <Text style={styles.statetext}> Cronômetro em pausa </Text>
+
+                </View>
+
+
+                <View style={styles.progressContainer}>
+
+
+                    <AnimatedCircularProgress
+                        size={160}
+                        width={7}
+                        fill={90}
+                        rotation={0}
+                        tintColor={Theme.colors.divider}
+                        backgroundColor={Theme.colors.primary}
+                        children={() => (
+                            <Text style={styles.progressText} >
+                                12:45
+                            </Text>
+                        )}
+                    />
+                </View>
+
+            </View>
 
             <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.primeryButton}>
@@ -65,6 +86,13 @@ export const Home = () => {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        gap: 36,
+        justifyContent: 'center'
+    },
+    titlegroup: {
+        gap: 24
+    },
     primeryButton: {
         backgroundColor: Theme.colors.primary,
         paddingVertical: 8,
@@ -92,11 +120,28 @@ const styles = StyleSheet.create({
     },
 
     buttonContainer: { flexDirection: 'row', justifyContent: "center", gap: 16 },
-    progressContainer: {alignItems: 'center'},
-    progressText: {color: Theme.colors.text,
+    progressContainer: { alignItems: 'center' },
+    progressText: {
+        color: Theme.colors.text,
         fontSize: Theme.fontSize.body,
         fontFamily: Theme.fonts.interBold
-}
+    },
+    titlecontainer: {
+        alignItems: 'center'
+    },
+    titletext: {
+        color: Theme.colors.text,
+        fontSize: Theme.fontSize.title,
+        fontFamily: Theme.fonts.interBold
+    },
 
+    statecontainer: {
+        alignItems: 'center'
+    },
+    statetext: {
+        color: Theme.colors.text,
+        fontSize: Theme.fontSize.body,
+        fontFamily: Theme.fonts.interRegular
+    }
 
 })
